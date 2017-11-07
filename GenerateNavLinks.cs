16a795850b426e9f6d18fbdf54f08de24503b647
mdotstrange <mdotstrange@gmail.com>
2017-11-07 15:42:34 -0800

    Update GenerateNavLinks.cs
    uses box collider center as collider position instead of transform attached to collider

--- GenerateNavLinks.cs
+++ GenerateNavLinks.cs
@@ -1,8 +1,7 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
-using Sirenix.OdinInspector;
 
 public class GenerateNavLinks : MonoBehaviour
 {
@@ -17,8 +16,9 @@ public class GenerateNavLinks : MonoBehaviour
     List<BoxCollider> ceilings = new List<BoxCollider>();
     public bool debugLines;
     public float wallConnectThreshold;
+    BoxCollider[] allBoxes;
 
-    [Button]
+
     public void DoGenerateLinks()
     {
         GetNavLinkTagTypes();
@@ -33,36 +33,38 @@ public class GenerateNavLinks : MonoBehaviour
         IfDistanceOkThenConnect(walls, walls);
         IfDistanceOkThenConnect(floors, floors);
         IfDistanceOkThenConnect(ceilings, ceilings);
-
     }
     public void GetNavLinkTagTypes()
     {
         navtags = GetComponentsInChildren<NavLinkTag>();
+        allBoxes = GetComponentsInChildren<BoxCollider>();
     }
 
     public void SeparateLinkTagTypes()
     {
-        for (int index = 0; index < navtags.Length; index++)
+        for (int index = 0; index < allBoxes.Length; index++)
         {
-            var i = navtags[index];
+            var box = allBoxes[index];
+
+            var i = box.gameObject.GetComponent<NavLinkTag>();
             var ii = i.RoomPieceType;
 
             if (ii == NavLinkTag.typo.Floor)
             {
-                var iii = i.gameObject.GetComponent<BoxCollider>();
-                floors.Add(iii);
+                //var iii = i.gameObject.GetComponents<BoxCollider>();
+                floors.Add(box);
             }
 
             if (ii == NavLinkTag.typo.Wall)
             {
-                var iii = i.gameObject.GetComponent<BoxCollider>();
-                walls.Add(iii);
+                //var iii = i.gameObject.GetComponents<BoxCollider>();
+                walls.Add(box);
             }
 
             if (ii == NavLinkTag.typo.Ceiling)
             {
-                var iii = i.gameObject.GetComponent<BoxCollider>();
-                ceilings.Add(iii);
+                //var iii = i.gameObject.GetComponents<BoxCollider>();
+                ceilings.Add(box);
             }
         }
     }
@@ -76,7 +78,6 @@ public class GenerateNavLinks : MonoBehaviour
             for (int index1 = 0; index1 < bList.Count; index1++)
             {
                 var ii = bList[index1];
-
                 ConnectTheLinks(i, ii);
             }
         }
@@ -93,11 +94,11 @@ public class GenerateNavLinks : MonoBehaviour
             {
                 var ii = bList[index1];
 
-                if(IsObjectCloseEnough(i,ii))
+                if (IsObjectCloseEnough(i, ii))
                 {
                     ConnectTheLinks(i, ii);
                 }
-              
+
             }
         }
 
@@ -105,24 +106,26 @@ public class GenerateNavLinks : MonoBehaviour
 
     public bool IsObjectCloseEnough(Collider a, Collider b)
     {
-        if(a.gameObject == b.gameObject)
+
+        if (string.Compare(a.gameObject.name, b.gameObject.name) == 0)
         {
             return false;
-        }    
+        }
 
-        var closestFromAToB = a.ClosestPoint(b.ClosestPoint(a.transform.position)); 
+        var boxCenter = a.GetComponent<BoxCollider>().center;
+        var aCenter = a.transform.TransformPoint(boxCenter);
+
+        var closestFromAToB = a.ClosestPoint(b.ClosestPoint(aCenter));
         var closestFromBToA = b.ClosestPoint(closestFromAToB);
         var distance = Vector3.Distance(closestFromAToB, closestFromBToA);
 
         if (distance <= wallConnectThreshold)
         {
             return true;
-        }
-        else
+        } else
         {
             return false;
         }
-
 
     }
 
@@ -136,7 +139,8 @@ public class GenerateNavLinks : MonoBehaviour
 
     public void GetClosestPointsToEachOther(Collider a, Collider b)
     {
-        closestPointFromAToB = a.ClosestPoint(b.ClosestPoint(a.transform.position));
+        var aCenter = GetBoxCenterPosition(a, a.transform);
+        closestPointFromAToB = a.ClosestPoint(b.ClosestPoint(aCenter));
         closestPointFromBToA = b.ClosestPoint(closestPointFromAToB);
     }
 
@@ -155,7 +159,9 @@ public class GenerateNavLinks : MonoBehaviour
 
     public void AdjustLinks(NavMeshLink link, Collider a, Collider b)
     {
-        var directionFromACenterToLinkStart = -(closestPointFromAToB - a.transform.position).normalized;
+        var aCenter = GetBoxCenterPosition(a, a.transform);
+
+        var directionFromACenterToLinkStart = -(closestPointFromAToB - aCenter).normalized;
         if (debugLines == true)
         {
             Debug.DrawRay(closestPointFromAToB, directionFromACenterToLinkStart, Color.green, 99);
@@ -164,8 +170,9 @@ public class GenerateNavLinks : MonoBehaviour
         Ray aRay = new Ray(closestPointFromAToB, directionFromACenterToLinkStart);
         var aPos = aRay.GetPoint(linkCompenstationAmount);
 
+        var bCenter = GetBoxCenterPosition(b, b.transform);
 
-        var directionFromBTransformToLinkEnd = -(closestPointFromBToA - b.transform.position).normalized;
+        var directionFromBTransformToLinkEnd = -(closestPointFromBToA - bCenter).normalized;
         if (debugLines == true)
         {
             Debug.DrawRay(closestPointFromBToA, directionFromBTransformToLinkEnd, Color.red, 99);
@@ -177,6 +184,12 @@ public class GenerateNavLinks : MonoBehaviour
 
         link.startPoint = a.transform.InverseTransformPoint(aPos);
         link.endPoint = a.transform.InverseTransformPoint(bPos);
+    }
+
+    public Vector3 GetBoxCenterPosition(Collider coll, Transform trans)
+    {
+        var box = coll.GetComponent<BoxCollider>().center;
+        return trans.transform.TransformPoint(box);
     }
 
 }
