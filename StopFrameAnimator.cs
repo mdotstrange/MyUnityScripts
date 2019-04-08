368f7df1f3a4e588bba412b2f863483573b71ca4
mdotstrange <mdotstrange@gmail.com>
2019-04-07 22:46:46 -0700

    Add files via upload
    Easy way to add a stop motion effect to all the animations on an object- just drop in the animator component and adjust the frame interval

---
+++ StopFrameAnimator.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StopFrameAnimator : MonoBehaviour
+{
+    public Animator Animator;
+    public int FrameInterval;
+    int liveFrameInterval;
+    bool isNormalSpeed;
+    public bool IsActive;
+
+    private void Update()
+    {
+        if(IsActive)
+        {
+            liveFrameInterval++;
+
+            if (liveFrameInterval >= FrameInterval)
+            {
+                liveFrameInterval = 0;
+
+                if (isNormalSpeed)
+                {
+                    Animator.speed = 0;
+                } 
+                else
+                {
+                    Animator.speed = 1;
+                }
+
+                isNormalSpeed = !isNormalSpeed;
+            }
+        }
+ 
+    }
+
+}
