81c8a46d6c5788a19ea0577696ac699c1272574c
M dot Strange <mdotstrange@gmail.com>
2019-11-08 17:04:20 -0800

    Add files via upload

---
+++ MoveChildrenToRoot.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveChildrenToRoot : MonoBehaviour
+{
+    [Header("Script moves children to scene root at runtime")]
+    [Space]
+    public bool DestroyThisAfterMoving;
+
+    List<Transform> children = new List<Transform>();
+
+    private void Awake()
+    {
+        children.Clear();
+
+        for (int index = 0; index < transform.childCount; index++)
+        {
+            var child = transform.GetChild(index);
+            children.Add(child);
+        }
+
+       
+        for (int index = 0; index < children.Count; index++)
+        {
+            var i = children[index];
+            i.parent = null;
+        }
+
+        if(DestroyThisAfterMoving)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+ 
+
+}
